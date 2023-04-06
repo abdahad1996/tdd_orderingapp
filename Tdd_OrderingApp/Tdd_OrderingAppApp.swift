4185e1ca68847223f91e484a2577e30e6444da2a
abdul ahad <abdul_ahad1996@live.com>
2023-04-06 16:56:54 +0500

    Initial Commit

---
+++ Tdd_OrderingApp/Tdd_OrderingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tdd_OrderingAppApp.swift
+//  Tdd_OrderingApp
+//
+//  Created by abdul ahad on 06/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct Tdd_OrderingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
